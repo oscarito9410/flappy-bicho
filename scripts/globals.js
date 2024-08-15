80fdb1b374bc91385849d0918b45109cfb494665
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2024-08-14 22:54:17 -0600

    added resources

---
+++ scripts/globals.js
@@ -0,0 +1,23 @@
+
+	// images
+	var bird_picture = "assets/bicho_cape_animated.png";
+	var tube1_picture = "assets/top candle.png";
+	var tube2_picture = "assets/bottom candle.png";
+	var ground_picture = "assets/ground1.png";
+	var bg_picture = "assets/background option 1.png";
+
+    // sounds
+	var wing_sfx = "audio/bicho_woooo.mp3";
+	var point_sfx = "audio/bicho_pump_it.mp3";
+	var hit_sfx = "audio/bicho_ded.mp3";
+	var die_sfx = "default/sfx_die.mp3";
+	var swooshing_sfx = "default/sfx_swooshing.mp3";
+
+    // titles
+	var game_title = "BICHO WILL FLY";
+	var game_instructions = "TOUCH TO PUMP IT";
+
+    // texts
+	var game_over = "GAMEOVER";
+	var high_score = "HIGH SCORE";
+	var try_again = "TOUCH TO TRY AGAIN";
---
+++ scripts/state.js
@@ -0,0 +1,20 @@
+
+ const gameAudio = new Audio('audio/bicho_power_up.mp3');
+
+ function initializeGameAudio() {
+      gameAudio.volume = 0.6;
+ }
+
+ window.onload = function() {
+        initializeGameAudio();
+ };
+
+window.addEventListener('gameStateChange', function(event) {
+	    const gameState = event.detail.state;
+	    if (gameState === 'start') {
+	        gameAudio.play().catch(error => console.error("Audio play failed: ", error));
+	    } else if (gameState === 'over') {
+	        gameAudio.pause();
+	        gameAudio.currentTime = 0;
+	    }
+});
